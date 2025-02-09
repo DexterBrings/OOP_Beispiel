825a03668dcc3c9bfc27daceca7450a86d06995d
Dexter Brings <dexter.brings@gmail.com>
2025-02-09 20:20:24 +0100

    Initial commit

---
+++ OOP_Beispiel/OOP_Beispiel/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace OOP_Beispiel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Car car = new Car()
+            {
+                Speed = 100,
+                Acceleration = 10,
+                BreakSpeed = 50,
+                DoorCounter = 4
+            };
+
+            Plane plane = new Plane();
+
+            plane.Speed = 250;
+            plane.Acceleration = 50;
+            plane.DoorCounter = 2;
+
+            Console.WriteLine("Car speed: " + car.Speed);
+            Console.WriteLine("Car acceleration: " + car.Acceleration);
+            Console.WriteLine("Car breakspeed: " + car.BreakSpeed);
+            Console.WriteLine("Car doorcounter: " + car.Speed);
+
+            Console.WriteLine("Plane speed: " + plane.Speed);
+            Console.WriteLine("Plane acceleratioin: " + plane.Speed);
+            Console.WriteLine("Plane doorcounter: " + plane.Speed);
+        }
+    }
+}
---
+++ OOP_Beispiel/OOP_Beispiel/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Beispiel
+{
+    abstract class Vehicle
+    {
+        public float Speed { get; set; }
+        public float Acceleration { get; set; }
+        public float BreakSpeed { get; set; }
+        public int DoorCounter { get; set; }
+    }
+}
